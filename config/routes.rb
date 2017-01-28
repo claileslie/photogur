9954f01824eca7f3becfa958176c289f6da19da2
CLAIRE LESLIE <c.leslie@live.ca>
2017-01-28 17:15:30 -0500

    fix show in controller and refactor routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
-
   root 'pictures#index'
 
-  get 'pictures' => 'pictures#index'
-
-  post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
-
-  get 'pictures/:id' => 'pictures#show', as:'picture'
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-  patch 'pictures/:id' => "pictures#update"
+  resources :pictures
 
 end
