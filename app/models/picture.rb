236a4850409d4e41806b2147da7286790672ba41
CLAIRE LESLIE <c.leslie@live.ca>
2017-02-02 16:08:29 -0500

    Add validations for title, url, artist

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,9 @@
 class Picture < ApplicationRecord
+
+  validates_presence_of :artist, :url, :title
+  validates_uniqueness_of :url
+  validates_length_of :title, minimum: 3, too_short: 'please enter at least 3 characters', maximum: 20, too_long: 'please pick a shorter name'
+
   def self.most_recent_five
     Picture.newest_first.limit(5)
   end
